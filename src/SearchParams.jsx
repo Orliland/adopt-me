5cef2304e3c528c0626b57e00f438ad1110ad841
Orlando Verdin <git@orli.land>
2024-10-06 15:58:18 -0700

    useState hook update

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Los Angeles, CA";
+  const [location, setLocation] = useState("Los Angeles, CA");
 
   return (
     <div className="search-params">
@@ -7,6 +9,7 @@ const SearchParams = () => {
         <label htmlFor="location">
           Location
           <input
+            onChange={(e) => setLocation(e.target.value)}
             id="location"
             type="text"
             value={location}
