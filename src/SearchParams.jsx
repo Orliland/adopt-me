7e6da755852563c4bb264ca75e62a23e4a1bc822
Orlando Verdin <git@orli.land>
2024-10-06 22:02:54 -0700

    mapping data and add animal breeds

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Los Angeles, CA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -15,6 +19,33 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            name="animal"
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            {ANIMALS.map((element, key) => (
+              <option key={key}>{element}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            name="breed"
+            id="breed"
+            value={breed}
+            disabled={breeds.length === 0}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            {breeds.map((element, key) => (
+              <option key={key}>{element}</option>
+            ))}
+          </select>
         </label>
       </form>
     </div>
